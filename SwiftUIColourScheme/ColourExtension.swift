5658dec690cdea6bef90dba193a6cd7225998a40
Silvia España <silviaespanagil@gmail.com>
2022-01-15 16:48:08 +0100

    Add public extension for Color

---
+++ SwiftUIColourScheme/ColourExtension.swift
@@ -0,0 +1,28 @@
+//
+//  ColourExtension.swift
+//  SwiftUIColourScheme
+//
+//  Created by Silvia España on 15/1/22.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    // By colours
+    static let wine = Color("Wine")
+    static let brightRed = Color("BrightRed")
+    static let red = Color("Red")
+    static let orange = Color("Orange")
+    static let yellow = Color("Yellow")
+    
+    // By Use
+    static let button = Color("Wine")
+    static let buttonLabel = Color("Yellow")
+    static let primaryTitle = Color("Wine")
+    static let primaryText = Color("BrightRed")
+    
+    // By Use DarkMode
+    static let primaryTitleDM = Color("Yellow")
+    
+}
