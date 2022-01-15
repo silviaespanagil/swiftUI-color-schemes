63ed6b9b42ddf994b8777936eaa85325d2ce1f31
Silvia España <silviaespanagil@gmail.com>
2022-01-15 14:32:57 +0100

    Add new colorScheme

---
+++ SwiftUIColourScheme/SwiftUIColourScheme/SwiftUIColourSchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColourSchemeApp.swift
+//  SwiftUIColourScheme
+//
+//  Created by Silvia España on 15/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColourSchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
